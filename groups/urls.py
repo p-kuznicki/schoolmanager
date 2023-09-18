796244f2d3a221c26249a5c59de291c4ace49b58
onibanan <onibanan@gmail.com>
2023-09-19 01:46:15 +0200

    dynamic groups and lesson views

--- groups/urls.py
+++ groups/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
         path('', views.group_list, name='group_list'),
+        path('group/<int:pk>/', views.group_lessons, name='group_lessons'),
 ]
--- groups/views.py
+++ groups/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Group
 
 
 # Create your views here.
 
 def group_list(request):
-    return render(request, 'groups/group_list.html', {})
+    groups = Group.objects.order_by('level')
+    return render(request, 'groups/group_list.html', {'groups':groups})
 
+def group_lessons(request, pk):
+    group = get_object_or_404(Group, pk=pk)
+    return render(request, 'groups/group_lessons.html', {'group':group})
